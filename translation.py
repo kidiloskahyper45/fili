edc69a09f13cb426d356c304c90798e0eb515225
VWO53 <64356181+VWO53@users.noreply.github.com>
2020-08-24 11:55:27 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -19,7 +19,7 @@ Channel:-© @TG_BotZ
     AFTER_SUCCESSFUL_UPLOAD_MSG = "https://t.me/fileConvertRobot"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nPlease rate me if you find me useful. https://t.me/tlgrmcbot?start=anydl_bot-bot \nUploaded in {} seconds."
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
-    SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in next 24 hr"
+    SAVED_CUSTOM_THUMB_NAIL = "✅Custom video / file thumbnail saved. This image will be stored permanently"
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."

